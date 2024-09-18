ff2d6b7cb8527638f60ca32c1ca3c2afeb38aea9
Nikita Dudnik (nek) <nikdudnik@gmail.com>
2024-09-18 23:45:59 +0200

    Initial

---
+++ index.mjs
@@ -0,0 +1,5 @@
+export { default as genericDecoder } from "./lib/genericDecoder.mjs";
+export { default as sdfDecoder } from "./lib/example/sdfDecoder.mjs";
+export * from "./lib/arrays.mjs";
+export * from "./lib/domains.mjs";
+export * from "./lib/props.mjs";
